3ea269d74fed774d88c3e9abbb2df5a92bc0366e
T.james <ftj16@outlook.com>
2025-01-01 22:58:44 +0800

    Add boost daytime network samples

---
+++ network/AsyncTcpDaytimeServer/AsyncTcpDaytimeServer.cpp
@@ -0,0 +1,136 @@
+﻿// AsyncTcpDaytimeServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2024 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <array>
+#include <ctime>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+    using namespace std; // For time_t, time and ctime;
+    std::array<char,128> buf;
+    time_t now = time(0);
+    ctime_s(buf.data(),buf.size(),  &now);
+    return buf.data();
+}
+
+class tcp_connection
+    : public std::enable_shared_from_this<tcp_connection>
+{
+public:
+    typedef std::shared_ptr<tcp_connection> pointer;
+
+    static pointer create(boost::asio::io_context& io_context)
+    {
+        return pointer(new tcp_connection(io_context));
+    }
+
+    tcp::socket& socket()
+    {
+        return socket_;
+    }
+
+    void start()
+    {
+        message_ = make_daytime_string();
+
+        boost::asio::async_write(socket_, boost::asio::buffer(message_),
+            boost::bind(&tcp_connection::handle_write, shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+    }
+
+    tcp_connection(boost::asio::io_context& io_context)
+        : socket_(io_context)
+    {
+    }
+
+    void handle_write(const boost::system::error_code& /*error*/,
+        size_t /*bytes_transferred*/)
+    {
+    }
+private:
+
+    tcp::socket socket_;
+    std::string message_;
+};
+
+class tcp_server
+{
+public:
+    tcp_server(boost::asio::io_context& io_context)
+        : io_context_(io_context),
+        acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        tcp_connection::pointer new_connection =
+            tcp_connection::create(io_context_);
+
+        acceptor_.async_accept(new_connection->socket(),
+            boost::bind(&tcp_server::handle_accept, this, new_connection,
+                boost::asio::placeholders::error));
+    }
+
+    void handle_accept(tcp_connection::pointer new_connection,
+        const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            new_connection->start();
+        }
+
+        start_accept();
+    }
+
+    boost::asio::io_context& io_context_;
+    tcp::acceptor acceptor_;
+};
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+        tcp_server server(io_context);
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ network/SyncTcpDaytimeClient/SyncTcpDaytimeClient.cpp
@@ -0,0 +1,71 @@
+﻿// SyncTcpDaytimeClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: client <host>" << std::endl;
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        tcp::resolver resolver(io_context);
+        tcp::resolver::results_type endpoints =
+            resolver.resolve(argv[1], "daytime");
+
+        tcp::socket socket(io_context);
+        boost::asio::connect(socket, endpoints);
+
+        for (;;)
+        {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            std::cout.write(buf.data(), len);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
